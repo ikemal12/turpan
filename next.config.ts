611d829035a7c20a77ad44ebed10561594a46a63
ikemal12 <iltar2005@gmail.com>
2025-07-26 15:44:05 +0100

    update config

--- next.config.ts
+++ next.config.ts
@@ -3,13 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     formats: ['image/webp', 'image/avif'],
-    minimumCacheTTL: 31536000,
+    minimumCacheTTL: 31536000, 
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-  },
-  
-  experimental: {
-    optimizeCss: true,
   },
   
   async headers() {
